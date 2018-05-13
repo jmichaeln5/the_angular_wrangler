89c066cb20b16c82b16c00113306f073c10010d5
Jon Norton <jmichaeln5@gmail.com>
2018-05-13 02:14:03 -0400

    Basic backend complete

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Connect To Database
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to a database'+config.database);
+});
+
+// On Error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error'+err);
+});
 
 const app = express();
 
@@ -15,8 +29,18 @@ const port = 3000;
 // CORS Middleware
 app.use(cors());
 
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Body Parser Middleware
 app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
 
 app.use('/users', users);
 
@@ -29,3 +53,12 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log('Server started on port' + port);
 });
+
+
+
+
+
+
+
+
+//
